b1e2b0b0f84533e73309070bc80c7c2bddb66fa9
eamama <parksh4283@gmail.com>
2024-12-06 00:28:21 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 load_dotenv
 
 from langchain_openai import ChatOpenAI
-chat_model = ChatOpenAI()
+#chat_model = ChatOpenAI()
 
-subject = "AI"
-result = chat_model.invoke(subject + "에 대한 시를 써줘.")
-print(result.content)
+#subject = "AI"
+#result = chat_model.invoke(subject + "에 대한 시를 써줘.")
+#print(result.content)
 
 import streamlit as st
 st.title("인공지능 시인")
